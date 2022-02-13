fd02cb8ae52241c2cc40d9b33ae40a891ea6b581
yabdoul <95439560+yabdoul@users.noreply.github.com>
2022-02-13 08:27:36 -0800

    Add files via upload

---
+++ registre/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<div class="login"></div>
+<label>01</label>
+<p id="info">infos personnel</p>
+
+<div class="trait"></div>
+<img src="Hello-World-earth.svg" id="logo" alt="">
+<h3>Mylogistics</h3>
+<p id="indication">
+    Entrer votre information pour faciliter l’utilisation de notre application 
+</p>
+<p id="grey">
+    respecter les contraints indiqué pour chaque champ
+</p>
+<form action="login.php" method="GET" >
+<input type="text" class ="input" id="nom" name="nom" id="" style="position: absolute;left: 17.27%;right: 55.94%;top: 29.86%;bottom: 62.78%;background: #FFFFFF;height: 53px;width: 343px;border: none;" placeholder="nom de l'entreprise">
+<input type="text" name="email" style="position: absolute;left: 17.27%;right: 55.94%;top: 41.53%;bottom: 51.11%;background: #FFFFFF;height :53px ; width: 340px;border:none ;" placeholder="Email">
+<input type="text" name="password" style="position: absolute;left: 17.27%;right: 55.94%;top: 52.78%;bottom: 39.86%;background: #FFFFFF;height: 53px;width: 343px; border: none;" placeholder="Password">
+<input type="text" name="domaine" style="position: absolute;left: 17.27%;right: 55.94%;top: 64.03%;bottom: 28.61%;background: #FFFFFF; border:none ; width :343px ; height:53px ;" placeholder="Domaine d'activité">
+<button type="submit" name="submit" style="position: absolute;left: 16.56%;right: 68.75%;top: 77.5%;bottom: 15.83%;background: #6DD5FA;border-radius: 15px; border: none;width: 188px ; height: 48px; cursor: pointer;" >inscription</button>
+<p style="position: absolute;
+left: 33.05%;
+right: 64.84%;
+top: 78.61%;
+bottom: 17.92%;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: normal;
+font-size: 12px;
+line-height: 14px;
+
+color: #000000;">ou</p>
+<img src="gmail.png" style="position: absolute;
+left: 37.03%;
+right: 60.62%;
+top: 78.61%;
+bottom: 18.33%;
+width : 30px ;
+height :22px ;
+cursor :pointer ; 
+">
+</form>
+</div>
+<div class="prog">
+
+</div>
+<div class="canva ">
+<img src="business-3d-man-jumping.png" id="illust">
+</div> 
+
+    <script src="script.js">
+
+
+    </script>
+</body>
+</html>
---
+++ registre/login.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_GET["submit"]))
+{
+    header('location : login.php') ; 
+}
+
+
+
+
+
+
+
+
+?>
